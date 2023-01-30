8f58bdfb0f91bf83f84727916fb183a3a3f0dbff
LindsayH <lindsaynhenderson@gmail.com>
2023-01-30 16:52:03 -0600

    css for Ball component

---
+++ src/Lotto/Lottery.js
@@ -0,0 +1,21 @@
+// component design
+
+import { Component } from "react"
+
+//should show 6 lotto balls
+//Value 1-40 generated when button is clicked
+export Component Lottery {
+props
+title:
+numBalls:
+maxNum:
+
+this.state.
+nums: []
+
+events 
+onCLick
+
+}
+
+export default Lottery
