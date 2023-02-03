8d300352ddac5a253d846b06aed052bf67f898b2
LindsayH <lindsaynhenderson@gmail.com>
2023-02-03 15:11:13 -0600

    move colors to parent component

--- src/ColorBoxes/BoxContainer.js
+++ src/ColorBoxes/BoxContainer.js
@@ -4,10 +4,14 @@ import "./boxContainer.css"
 
 class BoxContainer extends Component {
   static defaultProps = {
-    numBoxes : 18
+    numBoxes : 18,
+    allColors: ["purple", "magenta", "rebeccapurple", "pink"]
   }
   render() {
-    const boxes = Array.from({length: this.props.numBoxes}).map(() => (<Box/>))
+    const boxes = Array.from({length: this.props.numBoxes}).map(() => (
+    <Box colors={this.props.allColors}/>
+    ))
+
     return <div className="boxContainer">
       {boxes}
     </div>
