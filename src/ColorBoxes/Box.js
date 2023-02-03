a2605b50c475c3ee896882fc3ec3330f024519e3
LindsayH <lindsaynhenderson@gmail.com>
2023-02-03 13:37:12 -0600

    add box.css

--- src/ColorBoxes/Box.js
+++ src/ColorBoxes/Box.js
@@ -1,9 +1,14 @@
 import React, { Component} from 'react'
+import './box.css'
 
 class Box extends Component {
   constructor(props) {
     super(props) 
       this.state = {color: 'purple'}
+      this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick() {
+
     }
     render(){
       return (
