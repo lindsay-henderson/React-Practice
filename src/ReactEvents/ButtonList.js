64e96c599b24a497a39ce2da3ed53b72219f385b
LindsayH <lindsaynhenderson@gmail.com>
2023-02-15 10:02:15 -0600

    set background color for buttons

--- src/ReactEvents/ButtonList.js
+++ src/ReactEvents/ButtonList.js
@@ -5,11 +5,19 @@ class ButtonList extends Component {
   static defaultProps = {
     colors: ["#0E6BA8", "#FB4D3D", "#03CEA4","#E40066"]
   }
+  constructor(props) {
+  super(props)
+  this.state = {color: "yellow"}
+  }
+
+  changeColor(color) {
+
+  }
 
   render() {
     return (
       <section>
-      <div className="buttons">
+      <div className="buttons" style={{backgroundColor: this.state.color}}>         
         {this.props.colors.map(c => {
           const colorObj = {backgroundColor: c}
           return <button style = {colorObj}>Click Here!</button>
